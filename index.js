61e5367c29df1247c12731d0763a0b7b8edc4d65
yangyuke <yangyuke@hetao101.com>
2019-07-03 17:50:20 +0800

    npm publish

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 /**
  *Created by limbo <yykaue@qq.com> on 2019/7/3.
  */
+import deepCopy from './lib/deepCopy'
+import getEnv from './lib/getEnv'
+import objProxy from './lib/objProxy'
+import { setSession, getSession, pushSession } from './lib/sessionFn'
 
-export {}
-export default {}
+export { deepCopy }
+export { getEnv }
+export { objProxy }
+export { setSession }
+export { getSession }
+export { pushSession }
+export default {
+  deepCopy,
+  getEnv,
+  objProxy,
+  setSession,
+  getSession,
+  pushSession
+}
